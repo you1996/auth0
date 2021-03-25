ccfe4d8eeb7ccfeedc1dc9db06b5051d2dd225e8
unknown <youssribentaghalline@gmail.com>
2021-03-26 00:15:10 +0100

    hif

--- routes/templates/templates.go
+++ routes/templates/templates.go
@@ -7,9 +7,20 @@ import (
 	"path/filepath"
 )
 
-type data interface{}
+func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	cwd, _ := os.Getwd()
+	t, err := template.ParseFiles(filepath.Join(cwd, "./routes/"+tmpl+"/"+tmpl+".html"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, data1 interface{}, data2 interface{}) {
+func RenderTemplateForUser(w http.ResponseWriter, tmpl string, data1 interface{}, data2 interface{}) {
 	cwd, _ := os.Getwd()
 	t, err := template.ParseFiles(filepath.Join(cwd, "./routes/"+tmpl+"/"+tmpl+".html"))
 	if err != nil {
