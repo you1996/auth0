17abbc354ce2cc63d94fce1397f50eb17d06f643
unknown <youssribentaghalline@gmail.com>
2021-03-26 00:05:17 +0100

    hi

--- routes/templates/templates.go
+++ routes/templates/templates.go
@@ -7,14 +7,18 @@ import (
 	"path/filepath"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, data map[interface{}]interface{}) {
+type data interface{}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, data1 interface{}, data2 interface{}) {
 	cwd, _ := os.Getwd()
 	t, err := template.ParseFiles(filepath.Join(cwd, "./routes/"+tmpl+"/"+tmpl+".html"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err = t.Execute(w, data)
+
+	err = t.Execute(w, data1)
+	err = t.Execute(w, data2)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
